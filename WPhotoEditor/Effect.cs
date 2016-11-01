571f01c9f48879400c7d543b5e5092db2aea32e2
FirstVP <FirstVP@DESKTOP-L65NII0>
2016-11-01 04:40:47 +0300

    InitialCommit

---
+++ WPhotoEditor/Effect.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Media3D;
+
+namespace WPhotoEditor
+{
+
+    public class MainShaderEffect : ShaderEffect
+    {
+        public static readonly DependencyProperty InputProperty = ShaderEffect.RegisterPixelShaderSamplerProperty("Input", typeof(MainShaderEffect), 0);
+        public static readonly DependencyProperty BrightnessProperty = DependencyProperty.Register("Brightness", typeof(double), typeof(MainShaderEffect), new UIPropertyMetadata(((double)(0D)), PixelShaderConstantCallback(0)));
+        public static readonly DependencyProperty ContrastProperty = DependencyProperty.Register("Contrast", typeof(double), typeof(MainShaderEffect), new UIPropertyMetadata(((double)(0D)), PixelShaderConstantCallback(1)));
+        public static readonly DependencyProperty RedProperty = DependencyProperty.Register("Red", typeof(double), typeof(MainShaderEffect), new UIPropertyMetadata(((double)(0D)), PixelShaderConstantCallback(2)));
+        public static readonly DependencyProperty GreenProperty = DependencyProperty.Register("Green", typeof(double), typeof(MainShaderEffect), new UIPropertyMetadata(((double)(0D)), PixelShaderConstantCallback(3)));
+        public static readonly DependencyProperty BlueProperty = DependencyProperty.Register("Blue", typeof(double), typeof(MainShaderEffect), new UIPropertyMetadata(((double)(0D)), PixelShaderConstantCallback(4)));
+        public MainShaderEffect()
+        {
+            PixelShader pixelShader = new PixelShader();
+            pixelShader.UriSource = new Uri("_Effect.ps", UriKind.Relative);
+            this.PixelShader = pixelShader;
+
+            this.UpdateShaderValue(InputProperty);
+            this.UpdateShaderValue(BrightnessProperty);
+            this.UpdateShaderValue(ContrastProperty);
+            this.UpdateShaderValue(RedProperty);
+            this.UpdateShaderValue(GreenProperty);
+            this.UpdateShaderValue(BlueProperty);
+        }
+        public Brush Input
+        {
+            get
+            {
+                return ((Brush)(this.GetValue(InputProperty)));
+            }
+            set
+            {
+                this.SetValue(InputProperty, value);
+            }
+        }
+        public double Brightness
+        {
+            get
+            {
+                return ((double)(this.GetValue(BrightnessProperty)));
+            }
+            set
+            {
+                this.SetValue(BrightnessProperty, value);
+            }
+        }
+        public double Contrast
+        {
+            get
+            {
+                return ((double)(this.GetValue(ContrastProperty)));
+            }
+            set
+            {
+                this.SetValue(ContrastProperty, value);
+            }
+        }
+        public double Red
+        {
+            get
+            {
+                return ((double)(this.GetValue(RedProperty)));
+            }
+            set
+            {
+                this.SetValue(RedProperty, value);
+            }
+        }
+        public double Green
+        {
+            get
+            {
+                return ((double)(this.GetValue(GreenProperty)));
+            }
+            set
+            {
+                this.SetValue(GreenProperty, value);
+            }
+        }
+        public double Blue
+        {
+            get
+            {
+                return ((double)(this.GetValue(BlueProperty)));
+            }
+            set
+            {
+                this.SetValue(BlueProperty, value);
+            }
+        }
+    }
+}
--- WPhotoEditor/MainWindow.xaml.cs
+++ WPhotoEditor/MainWindow.xaml.cs
@@ -8,9 +8,12 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Effects;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Drawing;
+using Microsoft.Win32;
 
 namespace WPhotoEditor
 {
@@ -22,6 +25,55 @@ namespace WPhotoEditor
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Open_Click(object sender, RoutedEventArgs e)
+        {
+            ShowOpenDialog();    
+        }
+
+        private void ShowOpenDialog()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();           
+            openFileDialog.Filter = "Файлы изображений |*.bmp;*.jpg;*.gif;*.png";
+            openFileDialog.FilterIndex = 1;
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                Bitmap currentPicture = new Bitmap(openFileDialog.FileName);                
+                mainImage.Width = currentPicture.Width;
+                mainImage.Height = currentPicture.Height;
+                mainImage.Source = BitmapToBitmapSource(currentPicture);
+
+                MainShaderEffect effect = new MainShaderEffect();
+                effect.Brightness = 0.5;
+                mainImage.Effect = effect;
+
+                // addPicture(currentPicture);            
+                Title = openFileDialog.FileName;
+
+            }
+        }
+
+        public static BitmapSource BitmapToBitmapSource(Bitmap source)
+        {
+            BitmapSource bitSrc = null;
+            var hBitmap = source.GetHbitmap();
+
+            try
+            {
+                bitSrc = System.Windows.Interop.Imaging.CreateBitmapSourceFromHBitmap(
+                    hBitmap,
+                    IntPtr.Zero,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromWidthAndHeight (source.Width, source.Height));
+            }
+            catch (Exception e)
+            {
+                bitSrc = null;
+                return null;
+            }            
+            return bitSrc;
         }
     }
 }
